9797c3a8c54ecddd5f6f6d28076e917e49a72731
Mathias Lavaert <mathias.lavaert@dataminded.be>
2017-01-19 08:47:59 +0100

    Initial commit

---
+++ src/main/java/be/dataminded/nifi/plugins/GenerateOracleTableFetch.java
@@ -0,0 +1,192 @@
+package be.dataminded.nifi.plugins;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.components.AbstractConfigurableComponent;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.dbcp.DBCPService;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.logging.ComponentLog;
+import org.apache.nifi.processor.*;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.util.StandardValidators;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+
+@Tags({"database", "sql", "table"})
+@CapabilityDescription("Generate queries to extract all data from database tables")
+public class GenerateOracleTableFetch extends AbstractConfigurableComponent implements Processor {
+
+    static final Relationship REL_SUCCESS;
+
+    static final PropertyDescriptor DBCP_SERVICE;
+    static final PropertyDescriptor TABLE_NAME;
+    static final PropertyDescriptor COLUMN_NAMES;
+    static final PropertyDescriptor QUERY_TIMEOUT;
+    static final PropertyDescriptor NUMBER_OF_PARTITIONS;
+    static final PropertyDescriptor SPLIT_COLUMN;
+
+    private ComponentLog logger;
+    private String identifier;
+
+    private final List<PropertyDescriptor> propertyDescriptors;
+    private final Set<Relationship> relationships;
+
+    public GenerateOracleTableFetch() {
+        relationships = ImmutableSet.of(REL_SUCCESS);
+        propertyDescriptors = ImmutableList.of(DBCP_SERVICE,
+                                               TABLE_NAME,
+                                               COLUMN_NAMES,
+                                               QUERY_TIMEOUT,
+                                               NUMBER_OF_PARTITIONS,
+                                               SPLIT_COLUMN);
+    }
+
+    @Override
+    public void onTrigger(ProcessContext context, ProcessSessionFactory sessionFactory) throws ProcessException {
+        ProcessSession session = sessionFactory.createSession();
+
+        final DBCPService dbcpService = context.getProperty(DBCP_SERVICE).asControllerService(DBCPService.class);
+        final String tableName = context.getProperty(TABLE_NAME).getValue();
+        final String columnNames = context.getProperty(COLUMN_NAMES).getValue();
+        final String splitColumnName = context.getProperty(SPLIT_COLUMN).getValue();
+        final int numberOfFetches = Integer.parseInt(context.getProperty(NUMBER_OF_PARTITIONS).getValue());
+        final Integer queryTimeout = context.getProperty(QUERY_TIMEOUT).asTimePeriod(TimeUnit.SECONDS).intValue();
+
+        try {
+            String selectQuery = String.format("SELECT MIN(%s), MAX(%s), COUNT(*) FROM %s",
+                                               splitColumnName,
+                                               splitColumnName,
+                                               tableName);
+            long low, high, numberOfRecords;
+
+            // Fetch metadata from the database //
+            try (final Connection con = dbcpService.getConnection();
+                 final Statement statement = con.createStatement()) {
+                statement.setQueryTimeout(queryTimeout);
+
+                logger.debug("Executing {}", new Object[]{selectQuery});
+                ResultSet resultSet = statement.executeQuery(selectQuery);
+                if (resultSet.next()) {
+                    low = resultSet.getLong(1);
+                    high = resultSet.getLong(2);
+                    numberOfRecords = resultSet.getLong(3);
+                } else {
+                    logger.error("Something is very wrong here, one row (even if count is zero) should have been returned: {}", new Object[]{selectQuery});
+                    throw new SQLException("No rows returned from metadata query: " + selectQuery);
+                }
+            } catch (SQLException e) {
+                logger.error("Unable to execute SQL select query {} due to {}", new Object[]{selectQuery, e});
+                throw new ProcessException(e);
+            }
+
+            long chunks = Math.min(numberOfFetches, numberOfRecords);
+            long chunkSize = (high - low) / Math.max(chunks, 1);
+            for (int i = 0; i < chunks; i++) {
+                long min = (low + i * chunkSize);
+                long max = (i == chunks -1 ) ? high : Math.min((i + 1) * chunkSize - 1 + low, high);
+                String query = String.format("SELECT %s FROM %s WHERE %s BETWEEN %s AND %s",
+                                             columnNames,
+                                             tableName,
+                                             splitColumnName,
+                                             min,
+                                             max);
+                FlowFile sqlFlowFile = session.create();
+                sqlFlowFile = session.write(sqlFlowFile, out -> out.write(query.getBytes()));
+                session.transfer(sqlFlowFile, REL_SUCCESS);
+            }
+            session.commit();
+        } catch (final ProcessException pe) {
+            // Log the cause of the ProcessException if it is available
+            Throwable t = (pe.getCause() == null ? pe : pe.getCause());
+            logger.error("Error during processing: {}", new Object[]{t.getMessage()}, t);
+            session.rollback();
+            context.yield();
+        }
+    }
+
+
+    @Override
+    public void initialize(ProcessorInitializationContext context) {
+        this.logger = context.getLogger();
+        this.identifier = context.getIdentifier();
+    }
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return relationships;
+    }
+
+
+    @Override
+    protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return propertyDescriptors;
+    }
+
+    @Override
+    public String getIdentifier() {
+        return identifier;
+    }
+
+
+
+    static {
+        REL_SUCCESS = new Relationship.Builder()
+                .name("success")
+                .description("Successfully created FlowFile from SQL query result set.")
+                .build();
+
+        DBCP_SERVICE = new org.apache.nifi.components.PropertyDescriptor.Builder()
+                .name("Database Connection Pooling Service")
+                .description("The Controller Service that is used to obtain a connection to the database.")
+                .required(true)
+                .identifiesControllerService(DBCPService.class)
+                .build();
+
+        TABLE_NAME = new org.apache.nifi.components.PropertyDescriptor.Builder()
+                .name("Table Name")
+                .description("The name of the database table to be queried.")
+                .required(true)
+                .addValidator(StandardValidators.NON_EMPTY_VALIDATOR).build();
+
+        COLUMN_NAMES = new org.apache.nifi.components.PropertyDescriptor.Builder()
+                .name("Columns to Return")
+                .description("A comma-separated list of column names to be used in the query. If your database requires special treatment of the names (quoting, e.g.), each name should include such treatment. If no column names are supplied, all columns in the specified table will be returned.")
+                .required(false)
+                .defaultValue("*")
+                .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+                .build();
+
+        QUERY_TIMEOUT = new org.apache.nifi.components.PropertyDescriptor.Builder()
+                .name("Max Wait Time")
+                .description("The maximum amount of time allowed for a running SQL select query , zero means there is no limit. Max time less than 1 second will be equal to zero.")
+                .defaultValue("0 seconds")
+                .required(true)
+                .addValidator(StandardValidators.TIME_PERIOD_VALIDATOR)
+                .build();
+
+        NUMBER_OF_PARTITIONS = new PropertyDescriptor.Builder()
+                .name("number-of-partitions")
+                .displayName("Number of partitions")
+                .defaultValue("4")
+                .addValidator(StandardValidators.NON_NEGATIVE_INTEGER_VALIDATOR)
+                .build();
+
+        SPLIT_COLUMN = new PropertyDescriptor.Builder()
+                .name("split-column")
+                .displayName("Split column")
+                .required(true)
+                .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+                .addValidator(StandardValidators.NON_BLANK_VALIDATOR)
+                .build();
+    }
+}
---
+++ src/test/java/be/dataminded/nifi/plugins/TestGenerateOracleTableFetch.java
@@ -0,0 +1,164 @@
+package be.dataminded.nifi.plugins;
+
+
+import com.google.common.collect.Lists;
+import org.apache.nifi.controller.AbstractControllerService;
+import org.apache.nifi.dbcp.DBCPService;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.reporting.InitializationException;
+import org.apache.nifi.util.MockFlowFile;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.apache.nifi.util.file.FileUtils;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static be.dataminded.nifi.plugins.GenerateOracleTableFetch.REL_SUCCESS;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * Unit tests for the GenerateTableFetch processor.
+ */
+public class TestGenerateOracleTableFetch {
+
+    private TestRunner runner;
+    private GenerateOracleTableFetch processor;
+
+    private final static String DB_LOCATION = "target/db_gtf";
+
+    @BeforeClass
+    public static void setupBeforeClass() throws IOException {
+        System.setProperty("derby.stream.error.file", "target/derby.log");
+
+        // remove previous test database, if any
+        final File dbLocation = new File(DB_LOCATION);
+        try {
+            FileUtils.deleteFile(dbLocation, true);
+        } catch (IOException ioe) {
+            // Do nothing, may not have existed
+        }
+    }
+
+    @AfterClass
+    public static void cleanUpAfterClass() throws Exception {
+        try {
+            DriverManager.getConnection("jdbc:derby:" + DB_LOCATION + ";shutdown=true");
+        } catch (SQLNonTransientConnectionException e) {
+            // Do nothing, this is what happens at Derby shutdown
+        }
+        // remove previous test database, if any
+        final File dbLocation = new File(DB_LOCATION);
+        try {
+            FileUtils.deleteFile(dbLocation, true);
+        } catch (IOException ioe) {
+            // Do nothing, may not have existed
+        }
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        processor = new GenerateOracleTableFetch();
+        final DBCPService dbcp = new DBCPServiceSimpleImpl();
+        final Map<String, String> dbcpProperties = new HashMap<>();
+
+        runner = TestRunners.newTestRunner(GenerateOracleTableFetch.class);
+        runner.addControllerService("dbcp", dbcp, dbcpProperties);
+        runner.enableControllerService(dbcp);
+        runner.setProperty(GenerateOracleTableFetch.DBCP_SERVICE, "dbcp");
+    }
+
+
+    private void dropTestTable(Connection con) {
+        try (Statement statement = con.createStatement()) {
+            statement.execute("DROP TABLE TEST_QUERY_DB_TABLE");
+        } catch (SQLException ignored) {
+        }
+    }
+
+
+    @Test
+    public void testAddedRows() throws ClassNotFoundException, SQLException, InitializationException, IOException {
+        // load test data to database
+        final Connection con = ((DBCPService) runner.getControllerService("dbcp")).getConnection();
+        Statement stmt = con.createStatement();
+
+        dropTestTable(con);
+        stmt.execute("CREATE TABLE TEST_QUERY_DB_TABLE (id INTEGER NOT NULL, name VARCHAR(100))");
+        stmt.execute("INSERT INTO TEST_QUERY_DB_TABLE (id, name) VALUES (1, 'Joe Smith')");
+        stmt.execute("INSERT INTO TEST_QUERY_DB_TABLE (id, name) VALUES (25, 'John Doe')");
+        stmt.execute("INSERT INTO TEST_QUERY_DB_TABLE (id, name) VALUES (50, 'Joey Johnson')");
+        stmt.execute("INSERT INTO TEST_QUERY_DB_TABLE (id, name) VALUES (75, 'Jasper Smith')");
+        stmt.execute("INSERT INTO TEST_QUERY_DB_TABLE (id, name) VALUES (100, 'Jason Statham')");
+
+
+        int numberOfPartitions = 4;
+
+        runner.setProperty(GenerateOracleTableFetch.TABLE_NAME, "TEST_QUERY_DB_TABLE");
+        runner.setIncomingConnection(false);
+        runner.setProperty(GenerateOracleTableFetch.SPLIT_COLUMN, "ID");
+        runner.setProperty(GenerateOracleTableFetch.NUMBER_OF_PARTITIONS, String.valueOf(numberOfPartitions));
+        runner.run();
+
+        runner.assertAllFlowFilesTransferred(REL_SUCCESS, numberOfPartitions);
+
+        List<String> queries = Lists.newArrayList();
+        for (MockFlowFile flowFile : runner.getFlowFilesForRelationship(REL_SUCCESS)) {
+            queries.add(new String(flowFile.toByteArray()));
+        }
+
+        assertThat(queries).contains(
+                "SELECT * FROM TEST_QUERY_DB_TABLE WHERE ID BETWEEN 1 AND 24",
+                "SELECT * FROM TEST_QUERY_DB_TABLE WHERE ID BETWEEN 25 AND 48",
+                "SELECT * FROM TEST_QUERY_DB_TABLE WHERE ID BETWEEN 49 AND 72",
+                "SELECT * FROM TEST_QUERY_DB_TABLE WHERE ID BETWEEN 73 AND 100");
+
+
+        List<String> names = queries.stream().flatMap(query -> {
+                                                      try {
+                                                          List<String> results = Lists.newArrayList();
+                                                          ResultSet resultSet = stmt.executeQuery(query);
+                                                          while (resultSet.next()) {
+                                                              results.add(resultSet.getString("name"));
+                                                          }
+                                                          return results.stream();
+                                                      } catch (SQLException ignore) {
+                                                      }
+                                                      return new ArrayList<String>().stream();
+                                                  }
+        ).collect(Collectors.toList());
+        assertThat(names).containsExactly("Joe Smith", "John Doe", "Joey Johnson", "Jasper Smith", "Jason Statham");
+    }
+
+    /**
+     * Simple implementation only for ListDatabaseTables processor testing.
+     */
+    private class DBCPServiceSimpleImpl extends AbstractControllerService implements DBCPService {
+
+        @Override
+        public String getIdentifier() {
+            return "dbcp";
+        }
+
+        @Override
+        public Connection getConnection() throws ProcessException {
+            try {
+                Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+                return DriverManager.getConnection("jdbc:derby:" + DB_LOCATION + ";create=true");
+            } catch (final Exception e) {
+                throw new ProcessException("getConnection failed: " + e);
+            }
+        }
+    }
+}
