e2daec37764da6a15522ba45dab62343f18da4ec
Mathias Lavaert <mathias.lavaert@persgroep.be>
2017-10-24 15:34:09 +0200

    Fix bug in closing the input stream from the flow file

--- nifi-dataminded-processors-aws/src/main/java/be/dataminded/nifi/plugins/PutCloudWatchCountMetricAndAlarm.java
+++ nifi-dataminded-processors-aws/src/main/java/be/dataminded/nifi/plugins/PutCloudWatchCountMetricAndAlarm.java
@@ -16,13 +16,15 @@ package be.dataminded.nifi.plugins;
  */
 
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.util.*;
-
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.services.cloudwatch.model.*;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClient;
+import com.amazonaws.services.cloudwatch.model.Dimension;
+import com.amazonaws.services.cloudwatch.model.MetricDatum;
+import com.amazonaws.services.cloudwatch.model.PutMetricAlarmRequest;
+import com.amazonaws.services.cloudwatch.model.PutMetricDataRequest;
+import com.google.common.base.Throwables;
 import org.apache.commons.io.IOUtils;
 import org.apache.nifi.annotation.behavior.InputRequirement;
 import org.apache.nifi.annotation.behavior.InputRequirement.Requirement;
@@ -34,17 +36,17 @@ import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.logging.ComponentLog;
 import org.apache.nifi.processor.ProcessContext;
 import org.apache.nifi.processor.ProcessSession;
-import org.apache.nifi.processor.Relationship;
 import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processor.util.StandardValidators;
 import org.apache.nifi.processors.aws.AbstractAWSCredentialsProviderProcessor;
-
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.services.cloudwatch.AmazonCloudWatchClient;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.*;
 
 
 @SupportsBatching
@@ -61,8 +63,6 @@ public class PutCloudWatchCountMetricAndAlarm extends AbstractAWSCredentialsProv
 
     private final ComponentLog logger = getLogger();
 
-    public static final Set<Relationship> relationships = Collections.unmodifiableSet(
-            new HashSet<>(Arrays.asList(REL_SUCCESS, REL_FAILURE)));
 
     public static final PropertyDescriptor NAME_ELEMENT_TOTAL_COUNT = new PropertyDescriptor.Builder()
             .name("TotalCountElementName")
@@ -148,45 +148,6 @@ public class PutCloudWatchCountMetricAndAlarm extends AbstractAWSCredentialsProv
             .addValidator(new StandardValidators.StringLengthValidator(1, 255))
             .build();
 
-    public static final List<PropertyDescriptor> properties =
-            Collections.unmodifiableList(
-                    Arrays.asList(NAME_ELEMENT_TOTAL_COUNT, NAME_ELEMENT_TO_SUM, ENVIRONMENT, NAME_PREFIX_ALARM,
-                            ALARM_STATISTIC, ALARM_PERIOD, ALARM_EVALUATE_PERIODS, ALARM_COMPARISON_OPERATOR,
-                            ALARM_ACTION, REGION, AWS_CREDENTIALS_PROVIDER_SERVICE, TIMEOUT, SSL_CONTEXT_SERVICE,
-                            ENDPOINT_OVERRIDE, PROXY_HOST, PROXY_HOST_PORT)
-            );
-
-    @Override
-    protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
-        return properties;
-    }
-
-    @Override
-    public Set<Relationship> getRelationships() {
-        return relationships;
-    }
-
-    /**
-     * Create client using aws credentials provider. This is the preferred way for creating clients
-     */
-
-    @Override
-    protected AmazonCloudWatchClient createClient(ProcessContext processContext, AWSCredentialsProvider awsCredentialsProvider, ClientConfiguration clientConfiguration) {
-        getLogger().info("Creating client using aws credentials provider");
-        return new AmazonCloudWatchClient(awsCredentialsProvider, clientConfiguration);
-    }
-
-    /**
-     * Create client using AWSCredentials
-     *
-     * @deprecated use {@link #createClient(ProcessContext, AWSCredentialsProvider, ClientConfiguration)} instead
-     */
-    @Override
-    protected AmazonCloudWatchClient createClient(ProcessContext processContext, AWSCredentials awsCredentials, ClientConfiguration clientConfiguration) {
-        getLogger().debug("Creating client with aws credentials");
-        return new AmazonCloudWatchClient(awsCredentials, clientConfiguration);
-    }
-
     @Override
     public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException {
         FlowFile flowFile = session.get();
@@ -194,132 +155,134 @@ public class PutCloudWatchCountMetricAndAlarm extends AbstractAWSCredentialsProv
             return;
         }
 
-        long totalTableCount = 0;
-        long sumCount = 0;
-        String tableName = "";
-        String schemaName = "";
-        String source = "";
-        String tenantName = "";
-
         try {
-
-            InputStream inputStream = session.read(flowFile);
-            StringWriter writer = new StringWriter();
-            IOUtils.copy(inputStream, writer, "UTF-8");
-            String flowFileContent = writer.toString();
+            String flowFileContent = readFlowFileContent(session, flowFile);
 
             // The MergeContent controller will be configured to append the JSON content with commas
             // We have to surround this list with square brackets to become a valid JSON Array
-            String jsonContent = "[" + flowFileContent + "]";
+            JSONArray jsonArray = new JSONArray("[" + flowFileContent + "]");
+            JSONObject firstElement = jsonArray.getJSONObject(0);
 
-            JSONArray jsonArray = new JSONArray(jsonContent);
+            final long totalTableCount = firstElement.getLong(context.getProperty(NAME_ELEMENT_TOTAL_COUNT).getValue());
+            final long sumCount = takeSumOfColumn(context, jsonArray) ;
+            final String tableName = firstElement.getString(TABLE_NAME);
+            final String schemaName = firstElement.getString(SCHEMA_NAME);
+            final String source = firstElement.getString(SOURCE_NAME);
+            final String tenantName = firstElement.getString(TENANT_NAME);
+            final String environment = context.getProperty(ENVIRONMENT).getValue();
+            final String alarmPrefix = context.getProperty(NAME_PREFIX_ALARM).getValue();
+            final List<Dimension> dimensions = makeDimensions(tableName, schemaName, source, tenantName, environment);
 
-            Iterator iterator = jsonArray.iterator();
-
-            ArrayList<Long> counts = new ArrayList<>();
-
-            while (iterator.hasNext()) {
-                JSONObject o = (JSONObject) iterator.next();
-                counts.add(o.getLong(context.getProperty(NAME_ELEMENT_TO_SUM).getValue()));
-            }
-            sumCount = counts.stream().mapToLong(Long::longValue).sum();
-
-            JSONObject firstElement = (JSONObject) jsonArray.get(0);
-            totalTableCount = firstElement.getLong(context.getProperty(NAME_ELEMENT_TOTAL_COUNT).getValue());
-            tableName = firstElement.getString(TABLE_NAME);
-            schemaName = firstElement.getString(SCHEMA_NAME);
-            source = firstElement.getString(SOURCE_NAME);
-            tenantName = firstElement.getString(TENANT_NAME);
-
-
-        } catch (IOException e) {
-            logger.error("Something went wrong when trying to read the flowFile body: " + e.getMessage());
-        } catch (org.json.JSONException e) {
-            logger.error("Something when trying to parse the JSON body of the flowFile: " + e.getMessage());
-        } catch (Exception e) {
-            logger.error("something else went wrong in body processing of this FlowFile: " + e.getMessage());
-            session.transfer(flowFile, REL_FAILURE);
-        }
-
-        try {
-
-            String environment = context.getProperty(ENVIRONMENT).getValue();
-            String alarmPrefix = context.getProperty(NAME_PREFIX_ALARM).getValue();
-
-            Map<String, Long> metrics = new HashMap<>();
-            // first metric: this is the total count of the records that were exported
-            metrics.put("COUNT_", sumCount);
-            // second metric: this is the difference between the records exported
-            // and the total amount of records counted in the DB, should always be 0 !!!
-            // we take a margin into account because we can't be sure there won't be any deletes
-            // between counting and executing the queries
-            long diff = Math.abs(totalTableCount - sumCount);
-            double diffProcent = Math.round((diff / totalTableCount) * 1000);
-            metrics.put("DIFF_", (long) diffProcent);
-
-            ArrayList<Dimension> dimensions = new ArrayList<>();
-            dimensions.add(new Dimension().withName("tableName").withValue(tableName));
-            dimensions.add(new Dimension().withName("tenantName").withValue(tenantName));
-            dimensions.add(new Dimension().withName("sourceName").withValue(source));
-            dimensions.add(new Dimension().withName("schemaName").withValue(schemaName));
-            dimensions.add(new Dimension().withName("environment").withValue(environment));
-
-            for (Map.Entry<String, Long> metric : metrics.entrySet()) {
-                MetricDatum datum = new MetricDatum();
-                datum.setMetricName(metric.getKey() + tableName);
-                datum.setValue((double) metric.getValue());
-                datum.setUnit("Count");
-                datum.setDimensions(dimensions);
-
-                final PutMetricDataRequest metricDataRequest = new PutMetricDataRequest()
-                        .withNamespace("NIFI")
-                        .withMetricData(datum);
-
-                putMetricData(metricDataRequest);
-            }
-
-            // the alarm we create is a static one that will check if the diff is zero
-            String comparisonOperator = context.getProperty(ALARM_COMPARISON_OPERATOR).getValue();
-            String alarmStatistic = context.getProperty(ALARM_STATISTIC).getValue();
-            String alarmPeriod = context.getProperty(ALARM_PERIOD).getValue();
-            String alarmEvaluatePeriods = context.getProperty(ALARM_EVALUATE_PERIODS).getValue();
-            String alarmAction = context.getProperty(ALARM_ACTION).getValue();
-
-            PutMetricAlarmRequest putMetricAlarmRequest = new PutMetricAlarmRequest()
-                    .withMetricName("DIFF_" + tableName)
-                    .withAlarmName(environment + "_" + alarmPrefix + "_" + "DIFF_" + tableName)
-                    .withDimensions(dimensions)
-                    .withComparisonOperator(comparisonOperator)
-                    .withNamespace("NIFI")
-                    .withStatistic(alarmStatistic)
-                    .withPeriod(Integer.parseInt(alarmPeriod))
-                    .withEvaluationPeriods(Integer.parseInt(alarmEvaluatePeriods))
-                    .withThreshold((double) 0)
-                    //.withTreatMissingData("notBreaching") // aws java SDK has to be upgraded for this
-                    .withAlarmDescription("The daily Count Alarm for table " + tableName)
-                    .withActionsEnabled(true)
-                    .withAlarmActions(alarmAction);
-            putAlarmData(putMetricAlarmRequest);
+            putMetricData(totalTableCount, sumCount, tableName, dimensions);
+            createAlarm(context, tableName, environment, alarmPrefix, dimensions);
 
             session.transfer(flowFile, REL_SUCCESS);
-            getLogger().info("Successfully published cloudwatch metric for {}", new Object[]{flowFile});
-        } catch (final Exception e) {
-            getLogger().error("Failed to publish cloudwatch metric for {} due to {}", new Object[]{flowFile, e});
+            logger.info("Successfully published CloudWatch metric for {}", new Object[]{flowFile});
+        } catch (JSONException e) {
+            logger.error("Something when trying to parse the JSON body of the flowFile: " + e.getMessage(), e);
+        } catch (Exception e) {
+            logger.error("Failed to publish CloudWatch metric for {} due to: ", new Object[]{flowFile}, e);
             flowFile = session.penalize(flowFile);
             session.transfer(flowFile, REL_FAILURE);
         }
-
     }
 
-    protected PutMetricDataResult putMetricData(PutMetricDataRequest metricDataRequest) throws AmazonClientException {
-        final AmazonCloudWatchClient client = getClient();
-        final PutMetricDataResult result = client.putMetricData(metricDataRequest);
-        return result;
+    @Override
+    protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return Collections.unmodifiableList(
+                Arrays.asList(NAME_ELEMENT_TOTAL_COUNT, NAME_ELEMENT_TO_SUM, ENVIRONMENT, NAME_PREFIX_ALARM,
+                        ALARM_STATISTIC, ALARM_PERIOD, ALARM_EVALUATE_PERIODS, ALARM_COMPARISON_OPERATOR,
+                        ALARM_ACTION, REGION, AWS_CREDENTIALS_PROVIDER_SERVICE, TIMEOUT, SSL_CONTEXT_SERVICE,
+                        ENDPOINT_OVERRIDE, PROXY_HOST, PROXY_HOST_PORT)
+        );
     }
 
-    public PutMetricAlarmResult putAlarmData(PutMetricAlarmRequest metricAlarmRequest) {
-        final AmazonCloudWatchClient client = getClient();
-        final PutMetricAlarmResult result = client.putMetricAlarm(metricAlarmRequest);
-        return result;
+    @Override
+    protected AmazonCloudWatchClient createClient(ProcessContext processContext, AWSCredentials awsCredentials, ClientConfiguration clientConfiguration) {
+        logger.debug("Creating client with aws credentials");
+        return new AmazonCloudWatchClient(awsCredentials, clientConfiguration);
+    }
+
+    @Override
+    protected AmazonCloudWatchClient createClient(ProcessContext processContext, AWSCredentialsProvider awsCredentialsProvider, ClientConfiguration clientConfiguration) {
+        logger.debug("Creating client using aws credentials provider");
+        return new AmazonCloudWatchClient(awsCredentialsProvider, clientConfiguration);
+    }
+
+    private void createAlarm(ProcessContext context, String tableName, String environment, String alarmPrefix, List<Dimension> dimensions) {
+        // the alarm we create is a static one that will check if the diff is zero
+        String comparisonOperator = context.getProperty(ALARM_COMPARISON_OPERATOR).getValue();
+        String alarmStatistic = context.getProperty(ALARM_STATISTIC).getValue();
+        String alarmPeriod = context.getProperty(ALARM_PERIOD).getValue();
+        String alarmEvaluatePeriods = context.getProperty(ALARM_EVALUATE_PERIODS).getValue();
+        String alarmAction = context.getProperty(ALARM_ACTION).getValue();
+
+        PutMetricAlarmRequest putMetricAlarmRequest = new PutMetricAlarmRequest()
+                .withMetricName("DIFF_" + tableName)
+                .withAlarmName(environment + "_" + alarmPrefix + "_" + "DIFF_" + tableName)
+                .withDimensions(dimensions)
+                .withComparisonOperator(comparisonOperator)
+                .withNamespace("NIFI")
+                .withStatistic(alarmStatistic)
+                .withPeriod(Integer.parseInt(alarmPeriod))
+                .withEvaluationPeriods(Integer.parseInt(alarmEvaluatePeriods))
+                .withThreshold(0d)
+                //.withTreatMissingData("notBreaching") // aws java SDK has to be upgraded for this
+                .withAlarmDescription("The daily Count Alarm for table " + tableName)
+                .withActionsEnabled(true)
+                .withAlarmActions(alarmAction);
+
+        getClient().putMetricAlarm(putMetricAlarmRequest);
+    }
+
+    private void putMetricData(long totalTableCount, long sumCount, String tableName, List<Dimension> dimensions) {
+        // first metric: this is the total count of the records that were exported
+        MetricDatum count = new MetricDatum()
+                .withMetricName(String.format("COUNT_%s", tableName))
+                .withValue((double )sumCount)
+                .withUnit("Count")
+                .withDimensions(dimensions);
+
+        // second metric: this is the difference between the records exported
+        // and the total amount of records counted in the DB, should always be 0 !!!
+        // we take a margin into account because we can't be sure there won't be any deletes
+        // between counting and executing the queries
+        MetricDatum diff = new MetricDatum()
+                .withMetricName(String.format("DIFF_%s", tableName))
+                .withValue((double) Math.round((Math.abs(totalTableCount - sumCount) / totalTableCount) * 1000))
+                .withUnit("Count")
+                .withDimensions(dimensions);
+
+        getClient().putMetricData(new PutMetricDataRequest().withMetricData(count, diff).withNamespace("NIFI"));
+    }
+
+    private long takeSumOfColumn(ProcessContext context, JSONArray json) {
+        long sum = 0L;
+        for (Object record : json) {
+            JSONObject jsonObject = (JSONObject) record;
+            sum += jsonObject.getLong(context.getProperty(NAME_ELEMENT_TO_SUM).getValue());
+        }
+        return sum;
+    }
+
+    private List<Dimension> makeDimensions(String tableName, String schemaName, String source, String tenantName, String environment) {
+        List<Dimension> dimensions = new ArrayList<>();
+        dimensions.add(new Dimension().withName("tableName").withValue(tableName));
+        dimensions.add(new Dimension().withName("tenantName").withValue(tenantName));
+        dimensions.add(new Dimension().withName("sourceName").withValue(source));
+        dimensions.add(new Dimension().withName("schemaName").withValue(schemaName));
+        dimensions.add(new Dimension().withName("environment").withValue(environment));
+        return dimensions;
+    }
+
+    private String readFlowFileContent(ProcessSession session, FlowFile flowFile) {
+        try (InputStream inputStream = session.read(flowFile)) {
+            StringWriter writer = new StringWriter();
+            IOUtils.copy(inputStream, writer, "UTF-8");
+            return writer.toString();
+        } catch (IOException e) {
+            logger.error("Something went wrong trying to read the content of the flow-file: {}", new Object[]{flowFile}, e);
+            throw Throwables.propagate(e);
+        }
     }
 }
